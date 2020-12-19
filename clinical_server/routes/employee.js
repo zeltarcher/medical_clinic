155b4e00432d90878a5181aa7edce32c9c7e3875
zeltarcher <truongchiquang@gmail.com>
2020-12-19 02:10:03 -0600

    delete main

--- clinical_server/routes/employee.js
+++
@@ -1,9 +0,0 @@
-const employee=require("../controllers/doctorController")
-const express=require("express")
-const router=express.Router()
-
-router.route("/getall").get(employee.getAllEmployee);
-router.route("/create").post(employee.create);
-router.route("/get/:id").get(employee.findOne).put(employee.update).delete(employee.delete);
-
-module.exports=router;
--- clinical_server/routes/patientRoute.js
+++
@@ -1,9 +0,0 @@
-const patient=require("../controllers/patientController");
-const express=require("express");
-const router=express.Router();
-
-router.route('/getall').get(patient.getAllPatient);
-router.route('/create').post(patient.create);
-router.route('/get/:id').get(patient.findOne).put(patient.update)
-.delete(patient.delete);
-module.exports=router;
